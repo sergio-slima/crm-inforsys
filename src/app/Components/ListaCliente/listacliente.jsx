91723061bdd312585c8ce31ffb205b0f4d5b9594
unknown <sergio_silvalima@hotmail.com>
2021-12-14 16:40:34 -0300

    lista cliente firebase

--- src/app/Components/ListaCliente/listacliente.jsx
+++ src/app/Components/ListaCliente/listacliente.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './listacliente.css';
-import clientes from '../../Dados/clientes';
+// import clientes from '../../Dados/clientes';
+import firebase from '../../Config/firebase';
 
 function ListaClientes() {
+
+    const [clientes, setClientes] = useState([]);
+    let listaCli = [];
+
+    useEffect(() => {
+        firebase.firestore().collection('clientes').get()
+            .then(async (resultado) => {
+                await resultado.docs.forEach((doc) => {
+                    listaCli.push({
+                        id: doc.id,
+                        nome: doc.data().nome,
+                        email: doc.data().email,
+                        fone: doc.data().fone
+                    });
+                })
+                setClientes(listaCli);
+            })
+    }, []);
+
     return <table className="table table-hover table-bordered">
         <thead>
             <tr className="table-secondary">
@@ -17,7 +37,7 @@ function ListaClientes() {
                 clientes.map((cliente) => {
                     return <tr key={cliente.id}>
                         <th scope="row">{cliente.id}</th>
-                        <td>{cliente.name}</td>
+                        <td>{cliente.nome}</td>
                         <td>{cliente.email}</td>
                         <td>{cliente.fone}</td>
                     </tr>
