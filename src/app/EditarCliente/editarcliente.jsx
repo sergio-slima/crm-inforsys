41f2d364db3699375d0e317c7a2f1242fb30d6a8
unknown <sergio_silvalima@hotmail.com>
2021-12-22 18:00:36 -0300

    ajustes na tela de cliente

--- src/app/EditarCliente/editarcliente.jsx
+++ src/app/EditarCliente/editarcliente.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import Navbar from '../Components/Navbar/navbar';
 import './editarcliente.css';
-
 import firebase from '../Config/firebase';
 import 'firebase/firestore';
 
@@ -16,20 +15,22 @@ function EditarCliente(props) {
     const db = firebase.firestore();
 
     useEffect(() => {
-        firebase.firestore().collection('clientes').doc(props.match.params.id).get()
-            .then((resultado) => {
-                setNome(resultado.data().nome);
-                setEmail(resultado.data().email);
-                setFone(resultado.data().fone);
-            })
-    }, []);
+        firebase.firestore().collection('clientes').doc(props.match.params.id).get().then((resultado) => {
+            setNome(resultado.data().nome);
+            setEmail(resultado.data().email);
+            setFone(resultado.data().fone);
+        })
+    }, [])
 
     function AlterarCliente() {
+
         if (nome.length === 0) {
             setMensagem('Informe o nome');
-        } else if (email.length === 0) {
+        }
+        else if (email.length === 0) {
             setMensagem('Informe o e-mail');
-        } else {
+        }
+        else {
             db.collection('clientes').doc(props.match.params.id).update({
                 nome: nome,
                 email: email,
@@ -47,26 +48,30 @@ function EditarCliente(props) {
     return <div>
         <Navbar />
         <div className="container-fluid titulo">
+
             <div className="offset-lg-3 col-lg-6">
                 <h1>Editar Cliente</h1>
-
                 <form>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1">Código</label>
-                        <input type="text" value={props.match.params.id} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Codigo" disabled />
+                        <label htmlFor="exampleInputEmail1" className="form-label">Código</label>
+                        <input type="text" value={props.match.params.id} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" disabled />
                     </div>
+
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1">Nome</label>
-                        <input onChange={(e) => setNome(e.target.value)} value={nome} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nome" />
+                        <label htmlFor="exampleInputEmail1" className="form-label">Nome</label>
+                        <input onChange={(e) => setNome(e.target.value)} value={nome} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     </div>
+
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1">E-mail</label>
-                        <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail" />
+                        <label htmlFor="exampleInputEmail1" className="form-label">E-mail</label>
+                        <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     </div>
+
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1">Fone</label>
-                        <input onChange={(e) => setFone(e.target.value)} value={fone} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Fone" />
+                        <label htmlFor="exampleInputEmail1" className="form-label">Fone</label>
+                        <input onChange={(e) => setFone(e.target.value)} value={fone} type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     </div>
+
                     <div className="text-center">
                         <Link to="/app/home" className="btn btn-outline-primary btn-acao">Cancelar</Link>
                         <button onClick={AlterarCliente} type="button" className="btn btn-primary btn-acao">Salvar</button>
@@ -74,10 +79,11 @@ function EditarCliente(props) {
 
                     {mensagem.length > 0 ? <div className="alert alert-danger mt-2" role="alert">{mensagem}</div> : null}
                     {sucesso === 'S' ? <Navigate to='/app/home' /> : null}
+
                 </form>
             </div>
         </div>
-    </div>
+    </div>;
 }
 
 export default EditarCliente;
